efa163676027ba93bfb3f7b9e20e4cfc8958823e
Gilbert M Muneka <munekamgilbert@gmail.com>
2023-03-21 16:45:01 -0500

    updates of Main CPU registery

--- MainCpuDataService/src/main/java/edu/miu/maincpudataservice/MainCpuDataServiceApplication.java
+++ MainCpuDataService/src/main/java/edu/miu/maincpudataservice/MainCpuDataServiceApplication.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MainCpuDataServiceApplication implements CommandLineRunner {
     @Autowired
     IMetricService cpuServices;
