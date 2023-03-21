1a6c65b8db7fb440fd46defc3b83d56829e3bdd3
Gilbert M Muneka <munekamgilbert@gmail.com>
2023-03-21 12:48:34 -0500

    Update on IMetricService - Adding new methods

--- CpuDataService/src/main/java/edu/miu/cpudataservice/service/IMetricService.java
+++ CpuDataService/src/main/java/edu/miu/cpudataservice/service/IMetricService.java
@@ -1,11 +1,14 @@
 package edu.miu.cpudataservice.service;
 
+import edu.miu.cpudataservice.domain.Metric;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface IMetricService {
-    public  void getAndSendData(String url);
-    
+    void getAndSendData(String url);
+    Metric getData(String url);
+    void sendData(String url, Metric metric);
 
+    void save(Metric metric);
 
 }
