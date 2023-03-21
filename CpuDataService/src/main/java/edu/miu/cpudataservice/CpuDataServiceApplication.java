af962e59a1b513635b1ac97b4552b4f33afa3f4d
Gilbert M Muneka <munekamgilbert@gmail.com>
2023-03-21 16:02:23 -0500

    Setting up Registry and registering Cpu service to the registery

--- CpuDataService/src/main/java/edu/miu/cpudataservice/CpuDataServiceApplication.java
+++ CpuDataService/src/main/java/edu/miu/cpudataservice/CpuDataServiceApplication.java
@@ -6,19 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class CpuDataServiceApplication implements CommandLineRunner {
     @Autowired
     CpuService cpuService;
-    @Autowired
-    IMetricService ramService;
-
-    @Bean
-    CpuService cpuService() {
-        return new CpuService();
-    }
     public static void main(String[] args) {
         SpringApplication.run(CpuDataServiceApplication.class, args);
     }
--- CpuDataService/src/main/java/edu/miu/cpudataservice/service/IMetricService.java
+++ CpuDataService/src/main/java/edu/miu/cpudataservice/service/IMetricService.java
@@ -3,7 +3,6 @@ package edu.miu.cpudataservice.service;
 import edu.miu.cpudataservice.domain.Metric;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface IMetricService {
     Metric getData(String url);
     void sendData(String url, Metric metric);
