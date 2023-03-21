f1a05b1f21e09111a719b08c89773114f0f2cb34
Gilbert M Muneka <munekamgilbert@gmail.com>
2023-03-20 21:43:54 -0500

    Second Commit of Domains

---
+++ src/main/java/miu/edu/service/CpuDataService.java
@@ -0,0 +1,4 @@
+package miu.edu.service;
+
+public class CpuDataService {
+}
--- src/main/java/miu/edu/swaproject/SwaProjectApplication.java
+++ src/main/java/miu/edu/swaproject/SwaProjectApplication.java
@@ -1,13 +1,41 @@
 package miu.edu.swaproject;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class SwaProjectApplication {
+public class SwaProjectApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SwaProjectApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Receiver is running and waiting for messages");
+
+        RestTemplate restTemplate = new RestTemplate();
+        String apiUrl = "http://localhost:19999/api/v1/data?chart=disk_ops.disk0";
+        String response = restTemplate.getForObject(apiUrl, String.class);
+        System.out.println(response);
+
+    }
+
+
+
+
+    //System.out.println(response);
+
+
+//    RestTemplate restTemplate = new RestTemplate();
+//    restTemplate.getForObject("http://localhost:8080/swaproject/swaproject", String.class);
+
+//    @Bean
+//    public RestTemplate restTemplate(){
+//        return new RestTemplate();
+//    }
+//    restTemplate.getForObject("http://localhost:8080/swaproject/swaproject", String.class);
 }
