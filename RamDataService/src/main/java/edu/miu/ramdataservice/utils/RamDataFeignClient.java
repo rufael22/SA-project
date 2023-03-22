156fa2e1f8346b553210cdb6bae93f12b4b451a5
Luwam Abraham <luwam.yihdego@miu.edu>
2023-03-21 22:41:25 -0500

    Implementation of RamService

---
+++ RamDataService/src/main/java/edu/miu/ramdataservice/utils/RamDataFeignClient.java
@@ -0,0 +1,19 @@
+package edu.miu.ramdataservice.utils;
+
+
+import edu.miu.ramdataservice.domain.Metric;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "Main-Cpu-Data-Service", fallback = RamDataFeignClientFallback.class)
+public interface RamDataFeignClient {
+    @PostMapping("/cpu-data/send")
+    @CircuitBreaker(name = "cpu-data-feign-client-circuit-breaker", fallbackMethod = "saveDataLocally")
+    String sendRemoteData(@RequestBody Metric metric);
+
+    default String saveDataLocally(Metric metric, Throwable throwable) {
+        return "Data cannot be sent remotely because of an exception!";
+    }
+}
--- src/main/java/miu/edu/swaproject/SwaProjectApplication.java
+++ src/main/java/miu/edu/swaproject/SwaProjectApplication.java
@@ -1,15 +1,21 @@
 package miu.edu.swaproject;
 
-import miu.edu.swaproject.service.CpuService;
-import miu.edu.swaproject.service.IMetricService;
+import miu.edu.service.CpuService;
+import miu.edu.service.IMetricService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SwaProjectApplication implements CommandLineRunner {
+
+//    public SwaProjectApplication(CpuService cpuService) {
+//        this.cpuService = cpuService;
+//    }
     @Autowired CpuService cpuService;
     @Autowired IMetricService ramService;
 
@@ -23,9 +29,27 @@ public class SwaProjectApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        RestTemplate restTemplate = new RestTemplate();
         String apiUrl = "http://localhost:19999/api/v1/data?chart=system.cpu";
         apiUrl += "&after=-2&format=json&points=1";
+        String response = restTemplate.getForObject(apiUrl, String.class);
+        System.out.println(response);
+
         cpuService.getAndSendData(apiUrl);
     }
 
+
+
+
+    //System.out.println(response);
+
+
+//    RestTemplate restTemplate = new RestTemplate();
+//    restTemplate.getForObject("http://localhost:8080/swaproject/swaproject", String.class);
+
+//    @Bean
+//    public RestTemplate restTemplate(){
+//        return new RestTemplate();
+//    }
+//    restTemplate.getForObject("http://localhost:8080/swaproject/swaproject", String.class);
 }
