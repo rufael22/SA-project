d3b591003e2012175a489b947443712b29d68863
Gilbert M Muneka <munekamgilbert@gmail.com>
2023-03-22 00:31:01 -0500

    Network Services updated -> pending Qualifier

--- NetworkDataService/src/main/java/edu/miu/networkdataservice/NetworkDataServiceApplication.java
+++ NetworkDataService/src/main/java/edu/miu/networkdataservice/NetworkDataServiceApplication.java
@@ -1,13 +1,38 @@
 package edu.miu.networkdataservice;
 
+import edu.miu.networkdataservice.domain.NetworkData;
+import edu.miu.networkdataservice.service.NetworkDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 @SpringBootApplication
-public class NetworkDataServiceApplication {
+@EnableDiscoveryClient
+public class NetworkDataServiceApplication implements CommandLineRunner {
+    @Autowired
+    NetworkDataService networkDataService;
 
     public static void main(String[] args) {
         SpringApplication.run(NetworkDataServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                String apiUrl = "http://localhost:19999/api/v1/data?chart=system.ipv4";
+                apiUrl += "&after=-2&format=json&points=1";
+                NetworkData data = (NetworkData) networkDataService.getData(apiUrl);
+                if(data != null) networkDataService.sendData(data);
+            }
+        }, 0, 1000);
     }
 
 }
---
+++ NetworkDataService/src/main/java/edu/miu/networkdataservice/domain/NetworkData.java
@@ -0,0 +1,18 @@
+package edu.miu.networkdataservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class NetworkData implements Metric {
+
+    private Computer computer;
+    private Long time;
+    private double received;
+    private double sent;
+}
