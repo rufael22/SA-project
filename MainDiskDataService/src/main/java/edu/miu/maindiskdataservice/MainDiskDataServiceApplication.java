fe99b472f36e636060411973849c229be82abc0e
Rufael Yohannes <rufaelyohannes@Rufaels-MacBook-Pro.local>
2023-03-22 15:43:39 -0500

    implementing MainDiskDataService

--- MainDiskDataService/src/main/java/edu/miu/maindiskdataservice/MainDiskDataServiceApplication.java
+++ MainDiskDataService/src/main/java/edu/miu/maindiskdataservice/MainDiskDataServiceApplication.java
@@ -1,13 +1,40 @@
 package edu.miu.maindiskdataservice;
 
+import edu.miu.maindiskdataservice.domain.DiskData;
+import edu.miu.maindiskdataservice.service.DiskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 @SpringBootApplication
-public class MainDiskDataServiceApplication {
+@EnableDiscoveryClient
+public class MainDiskDataServiceApplication implements CommandLineRunner {
+    @Autowired
+    DiskService diskService;
 
     public static void main(String[] args) {
         SpringApplication.run(MainDiskDataServiceApplication.class, args);
     }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                String apiUrl = "http://localhost:19999/api/v1/data?chart=system.io";
+                apiUrl += "&after=-2&format=json&points=1";
+                DiskData data = (DiskData) diskService.getData(apiUrl);
+                if(data != null) diskService.save(data);
+            }
+        }, 0, 1000);
+    }
+
+
 
 }
