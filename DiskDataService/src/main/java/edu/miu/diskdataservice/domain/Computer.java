560d4a30a7d80398b072951602fc52984ec5dad5
Luwam Abraham <luwam.yihdego@miu.edu>
2023-03-22 15:42:57 -0500

    disk service working

--- DiskDataService/src/main/java/edu/miu/diskdataservice/domain/Computer.java
+++ DiskDataService/src/main/java/edu/miu/diskdataservice/domain/Computer.java
@@ -1,10 +1,14 @@
 package edu.miu.diskdataservice.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document
 public class Computer {
 
     private long id;
